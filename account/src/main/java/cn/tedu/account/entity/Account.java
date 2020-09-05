8a20c09bd5990bfd637759259448a95797f178db
Aclles <aclles@163.com>
2020-09-05 14:57:28 +0800

    新增注册中心,账户项目2020.09.05

---
+++ account/src/main/java/cn/tedu/account/entity/Account.java
@@ -0,0 +1,11 @@
+package cn.tedu.account.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+}
